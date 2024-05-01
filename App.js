e1ea9263da98e42a04e783c0f8a0b8ab38069b02
cshimm <cameronjshimmin@gmail.com>
2024-05-01 16:57:56 -0700

    basic breed name fetching

--- App.js
+++ App.js
@@ -1,20 +1,44 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {StyleSheet, Text, View} from 'react-native';
+import {QueryClient, QueryClientProvider, useQuery} from "@tanstack/react-query";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    const queryClient = new QueryClient()
+    return (
+        <QueryClientProvider client={queryClient}>
+            <Dogs/>
+        </QueryClientProvider>
+    );
 }
 
+const Dogs = () => {
+    const getDogs = async () => {
+        const response = await fetch("https://dogapi.dog/api/v2/breeds")
+        return response.json()
+    }
+    const {data, isLoading, isError} = useQuery({queryKey: ['dogs'], queryFn: getDogs})
+    if (isLoading) {
+        return (
+            <Text>Loading...</Text>
+        )
+    }
+    if (isError) {
+        return (
+            <Text>There was an error...</Text>
+        )
+    }
+    return (
+        <View style={styles.container}>
+            {
+                data.data.map(d => <Text key={d.id}>{d.attributes.name}</Text>)
+            }
+        </View>
+    )
+}
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
 });
