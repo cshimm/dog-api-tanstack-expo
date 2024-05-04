b91878dabf3fd52caa602d718cc13a8acfa1c346
cshimm <cameronjshimmin@gmail.com>
2024-05-03 19:12:03 -0700

    groups added

--- App.js
+++ App.js
@@ -16,6 +16,10 @@ const getFacts = async (numFacts) => {
     const response = await fetch(`https://dogapi.dog/api/v2/facts?limit=${numFacts}`)
     return response.json()
 }
+const getGroups = async () => {
+    const response = await fetch("https://dogapi.dog/api/v2/groups")
+    return response.json()
+}
 export default function App() {
     const queryClient = new QueryClient()
     const [selectedId, setSelectedId] = useState('')
@@ -27,11 +31,30 @@ export default function App() {
             <View style={styles.container}>
                 <Dogs handleDogClick={handleDogClicked}/>
                 <Facts numFacts={5}/>
+                <Groups/>
                 <Dog dogId={selectedId}/>
             </View>
         </QueryClientProvider>
     );
 }
+const Groups = () => {
+    const {data: groups, isLoading: groupsLoading, isError: groupsError} = useQuery({
+        queryKey: ['groups'],
+        queryFn: getGroups
+    });
+    if (groupsLoading) return <Text>Groups Loading...</Text>
+    if (groupsError) return <Text>Groups Error...</Text>
+
+    return (
+        <View>
+            <Text style={styles.heading}>Groups:</Text>
+            {
+                groups.data.map(group => <Text key={group.id}>{group.attributes.name}</Text>)
+            }
+        </View>
+    )
+}
+
 
 const Facts = ({numFacts}) => {
     const {data: facts, isLoading: factsLoading, isError: factsError} = useQuery({
@@ -43,7 +66,7 @@ const Facts = ({numFacts}) => {
     console.log(facts.data.length)
     return (
         <View>
-            <Text>Fact(s):</Text>
+            <Text style={styles.heading}>Fact(s):</Text>
             {
                 facts.data.map(fact => <Text key={fact.id}>{fact.attributes.body}</Text>)
             }
@@ -63,7 +86,7 @@ const Dog = ({dogId}) => {
             {
                 dog.data.attributes &&
                 <View>
-                    <Text>{dog.data.attributes.name}</Text>
+                    <Text style={styles.heading}>{dog.data.attributes.name}</Text>
                     <Text>{dog.data.attributes.description}</Text>
                 </View>
             }
@@ -84,6 +107,7 @@ const Dogs = ({handleDogClick}) => {
     }
     return (
         <View>
+            <Text style={styles.heading}>Dogs</Text>
             {
                 dogs.data.map(d => <Text onPress={() => handleDogClick(d.id)} key={d.id}>{d.attributes.name}</Text>)
             }
@@ -99,5 +123,9 @@ const styles = StyleSheet.create({
     },
     dog: {
         width: '50%'
+    },
+    heading: {
+        fontSize: 30,
+        fontWeight:'bold'
     }
 });
